e9353221f1159b3b341844c0a1f7e56750223b88
enzovonkunoschy@gmail.com <enzovonkunoschy@gmail.com>
2024-08-01 07:52:06 -0300

    Con subida de carnet

--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
 //alert("vinculado")
+function cu00(){
+    window.location.href = "http://localhost:3000";
+}
+function cu01(){
+    window.location.href = "http://localhost:3000";
+}
+function cu11(){
+    window.location.href = "http://localhost:3000/cu11"
+}
+function cu111(){
+    window.location.href = "http://localhost:3000/cu111"
+}
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import express from 'express'
+import multer from 'multer'
 
 const app = express()
 
 app.use(express.static('public'))
 app.set('view engine','ejs')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'public/images')
+    },
+    filename: function(req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage })
 
 app.get('/',(req,res)=>{
     
@@ -11,5 +23,26 @@ app.get('/',(req,res)=>{
     res.render('pages/index',{})
 })
 
+app.get('/cu01',(req,res)=>{
+    res.render('pages/index1',{})
+})
+
+app.get('/cu11',(req,res)=>{
+    res.render('pages/index11',{})
+})
+
+app.get('/cu111',(req,res)=>{
+    res.render('pages/index111',{})
+})
+ 
+app.post('/carnet',upload.single('carnet'),(req, res)=>{
+    console.log(req.body)
+    console.log(req.file)
+    res.render('pages/index',{})
+})
+/*
+app.post('/carnet',(req,res)=>{
+    res.send("Respuesta desde carnet.")
+})*/
 
 app.listen(3000,()=>{console.log("Server listen on port 3000")})
