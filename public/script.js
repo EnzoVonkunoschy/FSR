aaed0b96050d4e2c11f937f4884007957838f52b
enzovonkunoschy@gmail.com <enzovonkunoschy@gmail.com>
2024-08-03 19:12:56 -0300

    include de script con la plantilla ejs

--- public/script.js
+++ public/script.js
@@ -1,16 +1,3 @@
 //alert("vinculado")
-function cu00(){
-   
-    window.location.href = url + "/";
-}
-function cu01(){
-    window.location.href = url + "/cu1";
-}
-function cu11(){
-   
-    window.location.href = url + "/cu11"
-}
-function cu111(){
 
-    window.location.href = url + "/cu111"
-}
+
