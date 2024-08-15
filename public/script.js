27030bff783eb5b41c9dcebed9a617489bb37cf3
RepoLavalle <computos@lavallemendoza.gob.ar>
2024-08-15 13:19:10 -0300

    Con objeto volador

--- public/script.js
+++ public/script.js
@@ -1,3 +1,15 @@
 //alert("vinculado")
+let nroSec = 1
+var traza = {}
+function procesar(arg){
+   let obj = JSON.parse(arg.getAttribute('id'))
+    console.log(Object.keys(obj)[0])
+    let seccion = Object.keys(obj)[0]
+   traza = {...traza,...obj}
+   console.log(traza)
+   document.getElementById('seccion'+nroSec).style.display = 'none';
+   nroSec += 1;
+   document.getElementById('seccion'+nroSec).style.display = 'block';
 
+}
 
