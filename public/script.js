0eaf2b94002b44994f226f2017a082e58280f27c
RepoLavalle <computos@lavallemendoza.gob.ar>
2024-08-20 13:26:44 -0300

    hasta ss

--- public/script.js
+++ public/script.js
@@ -2,22 +2,43 @@
 let canSec = 3
 let nroSec = 0
 
+let secAct = 0
+let secAnt = 0
 var traza = {}
+
 function procesar(arg){
 
    let obj = JSON.parse(arg.getAttribute('id'))
+   console.log("obj")
+   console.log(obj)
+
+   trazar(obj)
 
    traza = {...traza,...obj}
+   console.log("traza")
    console.log(traza)
+   
+   document.getElementById('seccion'+secAct).style.display = 'none';
+   secAct=obj.ss
+   document.getElementById('seccion'+obj.ss).style.display = 'block';
+
+   /* 
    if(nroSec < canSec - 1){
       document.getElementById('seccion'+nroSec).style.display = 'none';
       nroSec += 1;
       document.getElementById('seccion'+nroSec).style.display = 'block';
    }else{
       window.location.href = url+"cu111"
-   }
+   }*/
 }
 
+function trazar(arg){
+   
+  var sel = document.getElementById('seleccionado').innerHTML
+  sel += arg[Object.keys(arg)[0]]+'<br>'
 
+  document.getElementById('seleccionado').innerHTML = sel
+
+}
    
 
